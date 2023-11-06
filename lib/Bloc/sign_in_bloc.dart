c800228a7f050f7078acc87eff934380d2fec1c9
System 16 <mittuparmar19@gmail.com>
2023-11-06 11:37:32 +0530

    Email And Password Validation Added,validate state added in bloc ,InvalidState removed

--- lib/Bloc/sign_in_bloc.dart
+++ lib/Bloc/sign_in_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:login_bloc/Bloc/sign_in_event.dart';
 import 'package:login_bloc/Bloc/sign_in_state.dart';
@@ -7,14 +8,21 @@ class SignInBloc extends Bloc <SignInEvent,SignInState>{
        SignInBloc():super(SignInInitialState()){
             
          on<SignInTextChangedEvent>((event, emit) {
-           if(event.emailValue == ""){
-             emit(SignInErrorState('Enter Your Email First'));
+           if(event.emailValue == "" && EmailValidator.validate(event.emailValue) == false){
+             emit(SignInErrorState('Enter Your Email First!'));
 
+           }
+           else if(event.passwordValue.length<8)
+             {
+               emit(SignInErrorState('Enter Correct Password!'));
+             }
+           else{
+             emit(SignInValidState());
            }
 
          });
          on<SignInSubmittedEvent>((event, emit) {
-           
+            emit (SignInLoadingState());
          });
        }
 }
--- lib/Bloc/sign_in_state.dart
+++ lib/Bloc/sign_in_state.dart
@@ -4,7 +4,7 @@ class SignInInitialState extends SignInState {}
 
 class SignInValidState extends SignInState {}
 
-class  SignInInvalidState extends SignInState{}
+
 
 class SignInErrorState extends SignInState {
   final String errorMsg;
